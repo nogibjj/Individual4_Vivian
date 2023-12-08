08d62d82c189b11bab7cea55651d2652680643fe
vivianzzzzz <143654445+vivianzzzzz@users.noreply.github.com>
2023-12-08 02:35:59 +0000

    update

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-"""
-Main cli or app entry point
-"""
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import openai
+import os
 
-from mylib.calculator import add
-import click
+app = Flask(__name__)
 
-#var=1;var=2
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_KEY")
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+
+@app.route("/team")
+def team():
+    selected = request.args.get("selectedOption")
+    ## TODO call OpenAI API, get a introduction
+    prompt = f"give me a short introduction for {selected}"
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0,
+    )
+    rslt = response.choices[0].message["content"]
+    return render_template("result.html", selected=selected, intro=rslt)
+
+
+def a():
+    assert 1 == 1
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    app.run(port=8000)
